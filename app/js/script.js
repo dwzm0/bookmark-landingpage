3ae6c14a82c75b14e461fb2aa06d01839666f1a2
dwzm0 <dwzm00@gmail.com>
2023-07-03 00:33:18 +0300

    view change logic refactor

--- app/js/script.js
+++ app/js/script.js
@@ -20,26 +20,14 @@ const img  = document.querySelector('.features__view-current .view img')
 const header = document.querySelector('.features__view-current .text h2')
 const text = document.querySelector('.features__view-current .text p')
 
-console.log(header)
-console.log(text)
+const changeView = function(link, viewSrc, viewHeader, viewText) {
+    link.addEventListener('click', () => {
+        img.src = viewSrc
+        header.textContent = viewHeader
+        text.textContent = viewText
+    })
+}
 
-links[0].addEventListener('click', () => {
-    img.src = ('../../images/illustration-features-tab-1.svg')
-    header.textContent = ('Bookmark in one click')
-    text.textContent = ('Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.')
-})
-
-links[1].addEventListener('click', () => {
-    img.src = ('../../images/illustration-features-tab-2.svg')
-    header.textContent = ('  Intelligent search')
-    text.textContent = (' Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.')
-})
-
-links[2].addEventListener('click', () => {
-    img.src = ('../../images/illustration-features-tab-3.svg')
-    header.textContent = ('Share your bookmarks')
-    text.textContent = ('Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.')
-})
-
-console.log(links);
-console.log(links[0]);
+changeView(links[0], '../../images/illustration-features-tab-1.svg', 'Bookmark in one click', 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.')
+changeView(links[1], '../../images/illustration-features-tab-2.svg', 'Intelligent search', 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.')
+changeView(links[2], '../../images/illustration-features-tab-3.svg', 'Share your bookmarks', 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.')
