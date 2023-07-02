a57065014cf74abe4295cfe38dcbe4f209298d69
dwzm0 <dwzm00@gmail.com>
2023-07-03 00:24:28 +0300

    Added view change logic

--- app/js/script.js
+++ app/js/script.js
@@ -14,3 +14,32 @@ if(mobileMenuClose) {
         document.body.classList.remove('opened', 'lock')
     })
 }
+
+const links = document.querySelector('.features__selection').children
+const img  = document.querySelector('.features__view-current .view img')
+const header = document.querySelector('.features__view-current .text h2')
+const text = document.querySelector('.features__view-current .text p')
+
+console.log(header)
+console.log(text)
+
+links[0].addEventListener('click', () => {
+    img.src = ('../../images/illustration-features-tab-1.svg')
+    header.textContent = ('Bookmark in one click')
+    text.textContent = ('Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.')
+})
+
+links[1].addEventListener('click', () => {
+    img.src = ('../../images/illustration-features-tab-2.svg')
+    header.textContent = ('  Intelligent search')
+    text.textContent = (' Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.')
+})
+
+links[2].addEventListener('click', () => {
+    img.src = ('../../images/illustration-features-tab-3.svg')
+    header.textContent = ('Share your bookmarks')
+    text.textContent = ('Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.')
+})
+
+console.log(links);
+console.log(links[0]);
