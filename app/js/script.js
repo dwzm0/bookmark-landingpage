3e1b67ba5f95cb4b84eeca12df645543a88aeaf0
dwzm0 <dwzm00@gmail.com>
2023-07-04 11:49:20 +0300

    desktop faq section

--- app/js/script.js
+++ app/js/script.js
@@ -71,3 +71,21 @@ const changeView = function(link, viewSrc, viewHeader, viewText) {
 changeView(links[0], '../../images/illustration-features-tab-1.svg', 'Bookmark in one click', 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.')
 changeView(links[1], '../../images/illustration-features-tab-2.svg', 'Intelligent search', 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.')
 changeView(links[2], '../../images/illustration-features-tab-3.svg', 'Share your bookmarks', 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.')
+
+
+const faqItems = document.querySelectorAll('.faq__item')
+
+console.log(faqItems);
+
+faqItems.forEach((item) => {
+    item.addEventListener('click', () => {
+       if (item.children[1].style.display == '') {
+            item.children[1].style.display = 'flex'
+            item.children[0].children[1].classList.add('arrow-transform')
+       }else if (item.children[1].style.display == 'flex') {
+            item.children[1].style.display = ''
+            item.children[0].children[1].classList.remove('arrow-transform')
+       }
+      
+    })
+})
